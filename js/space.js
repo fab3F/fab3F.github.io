2b57294017a584cc91566af6beaf6177cda8d18b
fab3F <fbnhaukede@gmail.com>
2022-12-18 20:17:02 +0100

    csv

---
+++ js/space.js
@@ -0,0 +1,10 @@
+let body = document.body,
+    html = document.documentElement;
+let complete_height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+let background = document.getElementById("content");
+let h = document.getElementById("home").clientHeight;
+let f = document.getElementById("footer").clientHeight;
+let he = complete_height - h - f - 2;
+let hei = 'min-height: ' + he.toString() + 'px;'
+background.setAttribute("style", hei);
