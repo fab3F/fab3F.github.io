6cf46cd7f574fbc2ff4940be62546d4351b8e870
fab3F <fbnhaukede@gmail.com>
2022-12-22 23:31:06 +0100

    sdfdfs

--- js/space.js
+++ js/space.js
@@ -1,14 +1,20 @@
 var space = window.setInterval(function(){
-    let body = document.body,
-        html = document.documentElement;
-    let complete_height = Math.max( body.scrollHeight, body.offsetHeight, 
-                        html.clientHeight, html.scrollHeight, html.offsetHeight );
-    let background = document.getElementById("content");
+    let c = document.getElementById("content").clientHeight;
     let h = document.getElementById("home").clientHeight;
-    let f = document.getElementById("footer").clientHeight;
-    let he = complete_height - h - f -2;
-    if (he < 960) {
-        let hei = 'min-height: ' + he.toString() + 'px;'
-        background.setAttribute("style", hei);
+    let f = document.getElementById("footer").clientHeight + 2; // +2 is for border from footer
+
+    let bodyH = c + h + f;
+
+    let head = document.getElementsByTagName("header")[0];
+    if(typeof head != 'undefined'){
+        bodyH = c + h + f + head.clientHeight;
     }
-  }, 500);
+
+    let windowH = window.innerHeight;
+    let diff = 0;
+    if(bodyH < windowH){
+        diff = windowH - bodyH;
+    }
+    let cH = 'min-height: ' + (c + diff).toString + 'px;';
+    c.setAttribute("style", cH);
+}, 500);
