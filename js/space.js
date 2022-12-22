b4cf71f68a40686c133a8a78a0115012c312b8dd
fab3F <fbnhaukede@gmail.com>
2022-12-22 22:41:18 +0100

    space

--- js/space.js
+++ js/space.js
@@ -1,12 +1,16 @@
-let body = document.body,
-    html = document.documentElement;
-let complete_height = Math.max( body.scrollHeight, body.offsetHeight, 
-                       html.clientHeight, html.scrollHeight, html.offsetHeight );
-let background = document.getElementById("content");
-let h = document.getElementById("home").clientHeight;
-let f = document.getElementById("footer").clientHeight;
-let he = complete_height - h - f - 2;
-if (he < 960) {
-    let hei = 'min-height: ' + he.toString() + 'px;'
-    background.setAttribute("style", hei);
+function genSapace(){
+    let body = document.body,
+        html = document.documentElement;
+    let complete_height = Math.max( body.scrollHeight, body.offsetHeight, 
+                        html.clientHeight, html.scrollHeight, html.offsetHeight );
+    let background = document.getElementById("content");
+    let h = document.getElementById("home").clientHeight;
+    let f = document.getElementById("footer").clientHeight;
+    let he = complete_height - h - f -2;
+    if (he < 960) {
+        let hei = 'min-height: ' + he.toString() + 'px;'
+        background.setAttribute("style", hei);
+    }
 }
+
+let space = setInterval(genSapace, 100);
