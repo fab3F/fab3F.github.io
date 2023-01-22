c92d771f5c427d583bfca443e2e84e44bfdcda70
fab3F <fbnhaukede@gmail.com>
2023-01-22 19:21:20 +0100

    commit

---
+++ js/neoncursor.js
@@ -0,0 +1,14 @@
+import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
+neonCursor({
+    el: document.getElementById('content'),
+    shaderPoints: 16,
+    curvePoints: 60,
+    curveLerp: 0.6,
+    radius1: 7,
+    radius2: 30,
+    velocityTreshold: 10,
+    sleepRadiusX: 100,
+    sleepRadiusY: 100,
+    sleepTimeCoefX: 0.0025,
+    sleepTimeCoefY: 0.0025
+})
