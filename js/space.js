e9b9260813f04c0fe3c5b356c55dcb3980a5aa1a
fab3F <fbnhaukede@gmail.com>
2023-01-24 12:00:48 +0100

    commit

--- js/space.js
+++ js/space.js
@@ -5,32 +5,23 @@ function space(){
     let header = document.getElementsByTagName("header")[0];
 
     if(content && home){
-        let c = content.clientHeight;
-        let h = home.clientHeight;
-        let f = footer.clientHeight + 2; // +2 is for border from footer
+        let c = content.offsetHeight;
+        let h = home.offsetHeight;
+        let f = footer.offsetHeight;
         let bodyH = c + h + f;
         if(header){
-            bodyH = c + h + f + header.clientHeight;
+            bodyH = c + h + f + header.offsetHeight;
         }
         let windowH = window.innerHeight;
         let diff = windowH - bodyH;
         let style = c + diff;
         content.style.minHeight = style + "px";
     }
-
-    // für die csv viewer tabelle
-    setTimeout(function(){
-        let holder = document.getElementsByClassName("wtHolder")[0];
-        if(holder){
-            holder.removeAttribute("style");
-        }
-    }, 1000);
     
 }
 
 space();
 window.addEventListener("resize", space);
-window.addEventListener("handsontable:render", space);
 
 let docTitle = document.title;
 window.addEventListener("blur", () => {
