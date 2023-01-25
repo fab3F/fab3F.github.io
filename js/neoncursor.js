488baf77d2a0809656e2345afdf5525349ec4351
fab3F <fbnhaukede@gmail.com>
2023-01-25 09:14:36 +0100

    update neoncursor radius

--- js/neoncursor.js
+++ js/neoncursor.js
@@ -1,15 +1,21 @@
-import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
+let width = screen.width;
+    let r = 100;
+    if(width < 800){
+      r = (r/800) * width;
+    }
 
-neonCursor({
-  el: document.getElementById('neoncursor'),
-  shaderPoints: 16,
-  curvePoints: 80,
-  curveLerp: 0.5,
-  radius1: 5,
-  radius2: 30,
-  velocityTreshold: 10,
-  sleepRadiusX: 100,
-  sleepRadiusY: 100,
-  sleepTimeCoefX: 0.0025,
-  sleepTimeCoefY: 0.0025
-})
+    import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
+
+    neonCursor({
+      el: document.getElementById('neoncursor'),
+      shaderPoints: 16,
+      curvePoints: 80,
+      curveLerp: 0.5,
+      radius1: 5,
+      radius2: 30,
+      velocityTreshold: 10,
+      sleepRadiusX: r,
+      sleepRadiusY: r,
+      sleepTimeCoefX: 0.0025,
+      sleepTimeCoefY: 0.0025
+    })
